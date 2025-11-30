b2c343d8f1d698cc8f46be42d257d99cc565b032
Zoheir Djennane <zoheir.djennane@adextechnology.dz>
2025-11-30 02:34:24 +0100

    fix

--- src/adexgenie-widget.js
+++ src/adexgenie-widget.js
@@ -873,7 +873,5 @@ import { Room } from 'livekit-client';
     }
   }
 
-  // Expose both the class and an instance for flexibility
-  window.AdexGenieWidget = AdexGenie;
   window.AdexGenie = new AdexGenie();
 })();
